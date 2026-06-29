b8ae1358b19eb6a3140b17694eaa519c6e262417
Yasaswi Sarma <ch.yasaswisarma@gmail.com>
2026-06-29 22:27:38 +0530

    Update P14.java

--- Striver_Dsa/Pattrens/P14.java
+++ Striver_Dsa/Pattrens/P14.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class P14{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of rows:");
+        System.out.print("Enter number of rows:");
         int n = sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(" * " );
+        int k=1;
+        int j,l;
+        for(int i=1;i<=n;i++){
+            for(l=k,j=1;j<=i;j++){
+                System.out.print(l);
+                l++;
             }
+            k=l;
             System.out.println(" ");
         }
         sc.close();
